5cf9ceeed25e9c38b53cd1fdd95c59f99be31bfa
Emanuel2001 <emanuel.zakanj@gmail.com>
2023-03-17 09:30:38 +0100

    Adding CRUD functionalities to the API

---
+++ Controllers/CarController.cs
@@ -0,0 +1,54 @@
+﻿using Web_API.Models;
+using Web_API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Web_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<Car> Get(int id) 
+        {
+            if (id < 0) 
+            {
+                return BadRequest();
+            }
+            return CarService.Get(id);
+        }
+        [HttpGet]
+        public ActionResult<List<Car>> GetAll() => CarService.GetAll();
+        [HttpPost]
+        public IActionResult Create(Car car) 
+        { 
+            if(car == null) 
+            { 
+                return BadRequest(); 
+            }
+            CarService.Create(car);
+            return CreatedAtAction(nameof(Get),car);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int id,Car car) 
+        {
+            if (id < 0 || car == null )
+            {
+                return BadRequest();
+            }
+            CarService.Update(id,car);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        {
+            if (id < 0)
+            {
+                return BadRequest();
+            }
+            CarService.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Models/Manufacturer.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_API.Models
+{
+    public class Manufacturer
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+    }
+}
